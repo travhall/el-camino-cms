8c17c34189b337c32996f65ecfe6b494a22b2400
Travis Hall <unkletas@gmail.com>
2025-01-05 21:30:25 -0600

    .env and database config

--- config/database.ts
+++ config/database.ts
@@ -1,61 +1,43 @@
 // config/database.ts
 import path from "path";
+import { parse } from "pg-connection-string";
 
 export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
-  const connections = {
-    mysql: {
+  if (client === "postgres") {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+    return {
       connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
+        client,
+        connection: {
+          host,
+          port: parseInt(port),
+          database,
+          user,
+          password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+          schema: env("DATABASE_SCHEMA", "public"),
+        },
+        pool: {
+          min: env.int("DATABASE_POOL_MIN", 0),
+          max: env.int("DATABASE_POOL_MAX", 5),
+          acquireTimeoutMillis: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+          createTimeoutMillis: 30000,
+          idleTimeoutMillis: 60000,
+          reapIntervalMillis: 1000,
+          createRetryIntervalMillis: 100,
         },
       },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    sqlite: {
+    };
+  }
+
+  return {
+    connection: {
+      client,
       connection: {
         filename: path.join(
           __dirname,
@@ -65,14 +47,6 @@ export default ({ env }) => {
         ),
       },
       useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
 };
